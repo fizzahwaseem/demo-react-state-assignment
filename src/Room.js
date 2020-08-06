0b81445892adb203a187c8759fb4f21117d82d3b
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-06 08:44:17 +0500

    updated ternary operater

--- src/Room.js
+++ src/Room.js
@@ -6,7 +6,7 @@ function Room() {
     let [isLit, setLit] = useState(false);
     let [age,setAge] = useState(24);
   return (
-    <div className={`room $ {isLit? 'lit' : 'dark'}`}>
+    <div className={`room ${isLit ? "lit" : "dark"}`}>
       This room is {isLit? 'lit' : 'dark'}
       <br/>
       Age: {age}
