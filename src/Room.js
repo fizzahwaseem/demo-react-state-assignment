38f5de36621d7a9c4209a6e0e5f4802ecfa2f285
fizzahwaseem <50530599+fizzahwaseem@users.noreply.github.com>
2020-08-07 07:32:07 +0500

    restore room.js

--- src/Room.js
+++ src/Room.js
@@ -1,27 +1,22 @@
 import React, { useState } from 'react';
 import './Room.css';
 
+
 function Room() {
     let [isLit, setLit] = useState(false);
-    let [temp,setTemp] = useState(22);
+    let [age,setAge] = useState(24);
   return (
-    <div 
-    className={`room ${isLit? "lit" : "dark"}`}
-    >
+    <div className={`room ${isLit? "lit" : "dark"}`}>
       This room is {isLit? 'lit' : 'dark'}
       <br/>
-      Room Temperature is: {temp}°C
+      Age: {age}
       <br/>
       <button onClick={ ()=>setLit(!isLit) } >Toggle Light</button>
       <br/>
-      <button onClick={ ()=>setLit(true)}>On</button>
-      <br/>
-      <button onClick={ ()=> setLit(false) }> Off </button>
-      <br/>
       <button onClick={ ()=> {
-        console.log("Increase Temperature arrow function")
-        setTemp(++temp)
-      }}>Increase Temperature</button>
+        console.log("Increase age arrow function")
+        setAge(++age)
+      }}>Increase age</button>
     </div>
   );
 }
